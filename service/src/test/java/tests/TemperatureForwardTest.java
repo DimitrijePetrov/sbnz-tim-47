d2d28b75c65e5d0f428fde7045581a9f2d5f6827
DimitrijePetrov <dimpet96@gmail.com>
2023-05-29 21:09:14 +0200

    feat: finished template and forward for temperature

--- service/src/test/java/tests/TemperatureForwardTest.java
+++ service/src/test/java/tests/TemperatureForwardTest.java
@@ -20,7 +20,7 @@ public class TemperatureForwardTest {
         KieContainer kieContainer = ks.getKieClasspathContainer();
         KieSession kSession = kieContainer.newKieSession(ksessionName);
 
-        LocalDateTime oneHourFromNow = LocalDateTime.now().plusHours(1);
+        LocalDateTime oneHourFromNow = LocalDateTime.parse("2023-05-29T20:13:24.089848700").plusHours(1);
 
         MeasuredHourlyWeatherData m1 = new MeasuredHourlyWeatherData(oneHourFromNow.minusDays(1), 24.0, 50, 11.0, 50, 1.1);
         MeasuredHourlyWeatherData m2 = new MeasuredHourlyWeatherData(oneHourFromNow.minusDays(2), 22.0, 50, 0.0, 50, 1.1);
