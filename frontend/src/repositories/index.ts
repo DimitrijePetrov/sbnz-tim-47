0a70960efc26982d910da8f42d860641f61acadc
Andrej <krtolinat@gmail.com>
2023-05-30 15:56:45 +0200

    Update index.ts

--- frontend/src/repositories/index.ts
+++ frontend/src/repositories/index.ts
@@ -30,11 +30,11 @@ export const useReadCurrentWeather = () =>
             historicalDataAxios.get(
                 `forecast?latitude=45.25&longitude=19.84&hourly=temperature_2m,weathercode,relativehumidity_2m,surface_pressure,windspeed_10m&daily=temperature_2m_max,temperature_2m_min&forecast_days=1&timezone=Europe%2FBerlin`
             ),
-        { refetchInterval: 86400 }
+        { refetchInterval: 2000 }
     )
 
 export const useReadForecast = () =>
-    useQuery(['forecast'], (): Promise<ForecastResponse> => mainAxios.get('forecast'), { refetchInterval: 86400 })
+    useQuery(['forecast'], (): Promise<ForecastResponse> => mainAxios.get('forecast'), { refetchInterval: 2000 })
 
 export const useReadWarnings = () =>
-    useQuery(['warnings'], (): Promise<WarningsResponse> => mainAxios.get('warnings'), { refetchInterval: 86400 })
+    useQuery(['warnings'], (): Promise<WarningsResponse> => mainAxios.get('warnings'), { refetchInterval: 2000 })
